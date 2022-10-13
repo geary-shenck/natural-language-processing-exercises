7cc7216e2a31c827e7724eaf81f2ed8b76f8b0f4
Geary Shenck <geary.shenck@gmail.com>
2022-10-13 13:01:47 -0500

    post lesson pre review

---
+++ prepare.py
@@ -0,0 +1,77 @@
+
+def remove_stopwords(article_processed,words_to_add=[],words_to_remove=[]):
+    ''' 
+    takes in string, and two lists
+    creates list of words to remove from nltk, modifies as dictated in arguements
+    prints result of processing
+    returns resulting string
+    '''
+    from nltk.corpus import stopwords
+    #create the stopword list
+    stopwords_list = stopwords.words("english")
+    #modify stopword list
+    [stopwords_list.append(word) for word in words_to_add]
+    [stopwords_list.remove(word) for word in words_to_remove]
+    #remove using stopword list
+    words = article_processed.split()
+    filtered_words = [w for w in words if w not in stopwords_list]
+    #filtered_words =[word for word in article_processed if word not in stopwords_list]
+    print("removed ",len(article_processed)-len(filtered_words), "words")
+    #join back
+    article_without_stopwords = " ".join(filtered_words)
+    return article_without_stopwords
+
+def lemmatize(article):
+    ''' 
+    input article
+    makes object, applies to string, and returns results
+    '''
+    import nltk
+    #create the lemmatizer
+    wnl = nltk.stem.WordNetLemmatizer()
+    #use lemmatizer
+    lemmatized = [wnl.lemmatize(word) for word in article.split()]
+    #join words back together
+    article_lemmatized = " ".join(lemmatized)
+    return article_lemmatized
+
+def stem(article):
+    ''' 
+    input string
+    create object, apply it to the each in string, rejoin and return
+    '''
+    import nltk
+    #create porter stemmer
+    ps = nltk.porter.PorterStemmer()
+    stems = [ps.stem(word) for word in article.split()]
+    #join words back together
+    article_stemmed = " ".join(stems)
+    return article_stemmed
+
+def tokenize(article0):
+    ''' 
+    input string
+    creates object, returns string after object affect
+    '''
+    import nltk
+    #create the tokenizer
+    tokenize = nltk.tokenize.ToktokTokenizer()
+    #use the tokenizer
+    article = tokenize.tokenize(article0,return_str=True)
+    return article
+
+def basic_clean(article0):
+    ''' 
+    input string
+    lowers cases, makes "normal" characters, and removes anything not expected
+    returns article
+    '''
+    import unicodedata
+    import re
+    #lower cases
+    article = article0.lower()
+    ## decodes to change to "normal" characters after encoding to ascii from a unicode normalize
+    article = unicodedata.normalize("NFKD",article).encode("ascii","ignore").decode("utf-8")
+    # removes anything not lowercase, number, single quote, or a space
+    article = re.sub(r'[^a-z0-9\'\s]','',article)
+    return article
